4f55c811646322edbd3486cd0fcb73217ddbaaa2
sasie59 <sasie59@gmail.com>
2021-07-07 23:37:10 +0800

    fix 命名 country => county

--- cypress/integration/TRA/tests/check.fare.tests.js
+++ cypress/integration/TRA/tests/check.fare.tests.js
@@ -33,29 +33,29 @@ export const ChooseTrainTicketTypeTests = () => {
 
 export const ChooseStartStationTests = () => {
   it('should choose start station\n(選擇出發站為臺北)', () => {
-    cy.get('#mainline').as('country');
+    cy.get('#mainline').as('county');
     cy.get('#city63000').as('city');
     cy.get('.startStation.ui-autocomplete-input').siblings('.icon.icon-list').click();
-    cy.get('@country').contains('常用').should('be.visible');
-    cy.get('@country').contains('基隆市').should('be.visible');
-    cy.get('@country').contains('新北市').should('be.visible');
-    cy.get('@country').contains('臺北市').should('be.visible').click();
-    cy.get('@country').contains('桃園市').should('be.visible');
-    cy.get('@country').contains('新竹縣').should('be.visible');
-    cy.get('@country').contains('新竹市').should('be.visible');
-    cy.get('@country').contains('苗栗縣').should('be.visible');
-    cy.get('@country').contains('臺中市').should('be.visible');
-    cy.get('@country').contains('彰化縣').should('be.visible');
-    cy.get('@country').contains('南投縣').should('be.visible');
-    cy.get('@country').contains('雲林縣').should('be.visible');
-    cy.get('@country').contains('嘉義縣').should('be.visible');
-    cy.get('@country').contains('嘉義市').should('be.visible');
-    cy.get('@country').contains('臺南市').should('be.visible');
-    cy.get('@country').contains('高雄市').should('be.visible');
-    cy.get('@country').contains('屏東縣').should('be.visible');
-    cy.get('@country').contains('臺東縣').should('be.visible');
-    cy.get('@country').contains('花蓮縣').should('be.visible');
-    cy.get('@country').contains('宜蘭縣').should('be.visible');
+    cy.get('@county').contains('常用').should('be.visible');
+    cy.get('@county').contains('基隆市').should('be.visible');
+    cy.get('@county').contains('新北市').should('be.visible');
+    cy.get('@county').contains('臺北市').should('be.visible').click();
+    cy.get('@county').contains('桃園市').should('be.visible');
+    cy.get('@county').contains('新竹縣').should('be.visible');
+    cy.get('@county').contains('新竹市').should('be.visible');
+    cy.get('@county').contains('苗栗縣').should('be.visible');
+    cy.get('@county').contains('臺中市').should('be.visible');
+    cy.get('@county').contains('彰化縣').should('be.visible');
+    cy.get('@county').contains('南投縣').should('be.visible');
+    cy.get('@county').contains('雲林縣').should('be.visible');
+    cy.get('@county').contains('嘉義縣').should('be.visible');
+    cy.get('@county').contains('嘉義市').should('be.visible');
+    cy.get('@county').contains('臺南市').should('be.visible');
+    cy.get('@county').contains('高雄市').should('be.visible');
+    cy.get('@county').contains('屏東縣').should('be.visible');
+    cy.get('@county').contains('臺東縣').should('be.visible');
+    cy.get('@county').contains('花蓮縣').should('be.visible');
+    cy.get('@county').contains('宜蘭縣').should('be.visible');
     cy.get('@city').contains('南港').should('be.visible');
     cy.get('@city').contains('松山').should('be.visible');
     cy.get('@city').contains('臺北').should('be.visible');
@@ -68,10 +68,10 @@ export const ChooseStartStationTests = () => {
 
 export const ChooseEndStationTests = () => {
   it('should choose end station\n(選擇抵達站為南投縣集集)', () => {
-    cy.get('#mainline').as('country');
+    cy.get('#mainline').as('county');
     cy.get('#city10008').as('city');
     cy.get('.endStation.ui-autocomplete-input').siblings('.icon.icon-list').click();
-    cy.get('@country').contains('南投縣').click();
+    cy.get('@county').contains('南投縣').click();
     cy.get('@city').contains('濁水').should('be.visible');
     cy.get('@city').contains('龍泉').should('be.visible');
     cy.get('@city').contains('集集').should('be.visible');
