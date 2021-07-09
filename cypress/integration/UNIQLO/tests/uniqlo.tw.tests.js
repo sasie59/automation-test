a0afe926c336a71525ed54786c11586421111eb8
sasie59 <sasie59@gmail.com>
2021-07-09 23:22:02 +0800

    登入輸入帳密(but拒絕受訪)

--- cypress/integration/UNIQLO/tests/uniqlo.tw.tests.js
+++ cypress/integration/UNIQLO/tests/uniqlo.tw.tests.js
@@ -10,6 +10,10 @@ export const LoginTests = () => {
   it('should login account secret\n(登入帳號密碼)', () => {
     cy.get('img[alt="登入"]').click({ force: true });
     cy.url().should('eq', 'https://www.uniqlo.com/tw/store/FSC01010E02.do');
-  })
-
+    cy.get('input[name="cust_eml_id"]').type(Cypress.env('account'));
+    cy.get('input[name="cust_eml_id"]').should('have.value', Cypress.env('account'))
+    cy.get('input[name="cust_pass"]').type(Cypress.env('password'));
+    cy.get('input[name="cust_pass"]').should('have.value', Cypress.env('password'));
+    // cy.get('input[title="登入"]').click();
+  });
 }
