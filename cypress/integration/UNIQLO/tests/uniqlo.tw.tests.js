7810e3f46608637c738cacb251c8682d6d39024c
sasie59 <sasie59@gmail.com>
2021-07-11 23:13:59 +0800

    旋轉木馬的test is next ok but 圓點有bug

--- cypress/integration/UNIQLO/tests/uniqlo.tw.tests.js
+++ cypress/integration/UNIQLO/tests/uniqlo.tw.tests.js
@@ -1,6 +1,6 @@
 
 export const GoToUniqloPageTests = () => {
-  it('should go to uniqlo index\n(前往uniqlo)', () => {
+  it.only('should go to uniqlo index\n(前往uniqlo)', () => {
     cy.visit('https://www.uniqlo.com/tw/');
     cy.get('a[title="UNIQLO"]').should('be.visible');
   });
@@ -40,5 +40,75 @@ export const MainTypeTests = () => {
     cy.get('#gnav_baby').contains('嬰幼兒首頁').should('be.visible');
   });
 }
+
+export const CarouselTests = () => {
+  it.only('should click arrow or dots will change img\n(點擊箭頭或圓點會改變圖片)', () => {
+    cy.get('#main-slider').as('carousel_1');
+    cy.get('@carousel_1').contains('Next').click({force: true});
+    cy.get('img[src="https://im.uniqlo.com/images/tw/uq/pc/img/feature/top/210709/210709_L1_hero_02.jpg"]').should('be.visible');
+    // cy.get('@carousel_1').contains('Prev').click();
+    // cy.get('.bx-pager.bx-default-pager').contains(4).click();
+    // cy.get('img[src="https://im.uniqlo.com/images/tw/uq/pc/img/feature/top/210709/210709_L1_hero_04.jpg"]').should('be.visible');
+    // cy.get('.bx-pager.bx-default-pager').contains(1).click();
+    // cy.get('img[src="https://im.uniqlo.com/images/tw/uq/pc/img/feature/top/210709/210709_L1_hero_01.jpg"]').should('be.visible');
+    cy.get('.bx-pager-item').contains(4).click();
+    // cy.get('img[src="https://im.uniqlo.com/images/tw/uq/pc/img/feature/top/210709/210709_L1_hero_03.jpg"]').should('be.visible');
+  });
+}
+
+export const SearchBarTests = () => {
+  it('should', () => {
+
+  });
+}
+
+export const StoreInformationTests = () => {
+  it('should', () => {
+
+  });
+}
+
+export const RegisteredTests = () => {
+  it('should', () => {
+
+  });
+}
+
+export const WomenAreaTests = () => {
+  it('should', () => {
+
+  });
+}
+
+export const MenAreaTests = () => {
+  it('should', () => {
+
+  });
+}
+
+export const KidsAreaTests = () => {
+  it('should', () => {
+
+  });
+}
+
+export const BabyAreaTests = () => {
+  it('should', () => {
+
+  });
+}
+
+export const CommodityInformationTests = () => {
+  it('should', () => {
+
+  });
+}
+
+export const ShoppingCartTests = () => {
+  it('should', () => {
+
+  });
+}
+
 
 
