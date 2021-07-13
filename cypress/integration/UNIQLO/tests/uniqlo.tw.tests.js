851aac497381ec085d06cbf1d00ce6817d54be36
sasie59 <sasie59@gmail.com>
2021-07-14 01:58:20 +0800

    有bug 要用一個斷言式去規避this.head(前往首頁那報錯)

--- cypress/integration/UNIQLO/tests/uniqlo.tw.tests.js
+++ cypress/integration/UNIQLO/tests/uniqlo.tw.tests.js
@@ -5,15 +5,16 @@ export const DefineElement = () => {
   })
 }
 
-export const GoToUniqloPageTests = () => {
-  it.only('should go to uniqlo index\n(前往uniqlo)', () => {
+export function GoToUniqloPageTests() {
+  it('should go to uniqlo index\n(前往uniqlo)', () => {
     cy.visit('https://www.uniqlo.com/tw/');
     cy.get('a[title="UNIQLO"]').should('be.visible');
   });
 }
 
+
 export const LoginTests = () => {
-  it.skip('should login account secret\n(登入帳號密碼)', () => {
+  it('should login account secret\n(登入帳號密碼)', () => {
     cy.get('img[alt="登入"]').click({ force: true });
     cy.url().should('eq', 'https://www.uniqlo.com/tw/store/FSC01010E02.do');
     cy.get('input[name="cust_eml_id"]').type(Cypress.env('account'));
--- cypress/integration/UNIQLO/uniqloTw.spec.js
+++ cypress/integration/UNIQLO/uniqloTw.spec.js
@@ -4,20 +4,20 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   return false;
 });
 
-describe('台灣Uniqlo', () => {
+describe('前往台灣Uniqlo', () => {
   tests.GoToUniqloPageTests();
-  describe('登入帳號', () => {
-    tests.DefineElement();
-    tests.LoginTests();
-    tests.MainTypeTests();
-    tests.CarouselTests();
-    tests.SearchBarTests();
-    tests.StoreInformationTests();
-    tests.WomenAreaTests();
-    tests.MenAreaTests();
-    tests.KidsAreaTests();
-    tests.BabyAreaTests();
-    tests.CommodityInformationTests();
-    tests.ShoppingCartTests();
-  });
+});
+describe('行為測試', () => {
+  tests.DefineElement();
+  tests.LoginTests();
+  tests.MainTypeTests();
+  tests.CarouselTests();
+  tests.SearchBarTests();
+  tests.StoreInformationTests();
+  tests.WomenAreaTests();
+  tests.MenAreaTests();
+  tests.KidsAreaTests();
+  tests.BabyAreaTests();
+  tests.CommodityInformationTests();
+  tests.ShoppingCartTests();
 });
