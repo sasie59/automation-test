81cb40f6953a4fde2896faf3d5d7d1db3d81eef0
sasie59 <sasie59@gmail.com>
2021-07-28 23:51:54 +0800

    todo英翻中

--- cypress/integration/1-getting-started/todo.spec.js
+++ cypress/integration/1-getting-started/todo.spec.js
@@ -5,139 +5,132 @@
 // This spec file contains a variety of sample tests
 // for a todo list app that are designed to demonstrate
 // the power of writing tests in Cypress.
-//
+
 // To learn more about how Cypress works and
 // what makes it such an awesome testing tool,
 // please read our getting started guide:
-// https://on.cypress.io/introduction-to-cypress
+//on.cypress.io/introduction-to-cypress
 
-describe('example to-do app', () => {
+https: describe("example to-do app", () => {
   beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
-    cy.visit('https://example.cypress.io/todo')
-  })
+    // Cypress 開始時每個測試都是空白的
+    // 所以我們必須告訴它使用 `cy.visit()` 命令訪問我們的網站
+    // 因為我們想在所有測試開始時訪問相同的 URL，
+    // 我們將它包含在我們的 beforeEach 函數中，
+    // 以便它在每次測試之前運行
 
-  it('displays two todo items by default', () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
-    cy.get('.todo-list li').should('have.length', 2)
+    cy.visit("https://example.cypress.io/todo");
+  });
 
-    // We can go even further and check that the default todos each contain
-    // the correct text. We use the `first` and `last` functions
-    // to get just the first and last matched elements individually,
-    // and then perform an assertion with `should`.
-    cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
-    cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
-  })
+  it("displays two todo items by default", () => {
+    // 我們使用 `cy.get()` 命令來獲取與選擇器匹配的所有元素。
+    // 然後，我們使用 `should` 來斷言有兩個匹配的項目，這是兩個默認項目。
 
-  it('can add new todo items', () => {
-    // We'll store our item text in a variable so we can reuse it
-    const newItem = 'Feed the cat'
+    cy.get(".todo-list li").should("have.length", 2);
 
-    // Let's get the input element and use the `type` command to
-    // input our new list item. After typing the content of our item,
-    // we need to type the enter key as well in order to submit the input.
-    // This input has a data-test attribute so we'll use that to select the
-    // element in accordance with best practices:
-    // https://on.cypress.io/selecting-elements
-    cy.get('[data-test=new-todo]').type(`${newItem}{enter}`)
+    // 我們可以更進一步檢查每個默認的待辦事項是否包含正確的文本。我們使用
+    // `first` 和 `last` 函數分別獲取第一個和最後一個匹配的元素，
+    // 然後使用 `should` 執行斷言。
 
-    // Now that we've typed our new item, let's check that it actually was added to the list.
-    // Since it's the newest item, it should exist as the last element in the list.
-    // In addition, with the two default items, we should have a total of 3 elements in the list.
-    // Since assertions yield the element that was asserted on,
-    // we can chain both of these assertions together into a single statement.
-    cy.get('.todo-list li')
-      .should('have.length', 3)
+    cy.get(".todo-list li").first().should("have.text", "Pay electric bill");
+    cy.get(".todo-list li").last().should("have.text", "Walk the dog");
+  });
+
+  it("can add new todo items", () => {
+    // 我們將我們的項目文本存儲在一個變量中，以便我們可以重用它
+
+    const newItem = "Feed the cat";
+    // 讓我們獲取 input 元素並使用 `type` 命令輸入我們的新列表項。
+    // 輸入我們項目的內容後，我們還需要輸入回車鍵以提交輸入。
+    // 此輸入具有 data-test 屬性，因此我們將使用它來根據最佳實踐選擇元素：
+
+    //on.cypress.io/selecting-elements
+
+    https: cy.get("[data-test=new-todo]").type(`${newItem}{enter}`);
+
+    // 現在我們已經輸入了我們的新項目，讓我們檢查它是否真的被添加到列表中。
+    // 由於它是最新的項目，它應該作為列表中的最後一個元素存在。
+    // 此外，使用兩個默認項，我們應該在列表中總共有 3 個元素。
+    // 由於斷言產生了被斷言的元素，我們可以將這兩個斷言鏈接到一個語句中。
+
+    cy.get(".todo-list li")
+      .should("have.length", 3)
       .last()
-      .should('have.text', newItem)
-  })
+      .should("have.text", newItem);
+  });
 
-  it('can check off an item as completed', () => {
-    // In addition to using the `get` command to get an element by selector,
-    // we can also use the `contains` command to get an element by its contents.
-    // However, this will yield the <label>, which is lowest-level element that contains the text.
-    // In order to check the item, we'll find the <input> element for this <label>
-    // by traversing up the dom to the parent element. From there, we can `find`
-    // the child checkbox <input> element and use the `check` command to check it.
-    cy.contains('Pay electric bill')
+  it("can check off an item as completed", () => {
+    // 除了使用`get` 命令通過選擇器獲取元素之外，我們還可以使用`contains` 命令通過其內容獲取元素。
+    // 但是，這將產生 <label>，它是包含文本的最低級別元素。為了檢查項目，
+    // 我們將通過遍歷 dom 到父元素來找到此 <label> 的 <input> 元素。從那裡，我們可以`find`
+    // 子復選框<input> 元素並使用`check` 命令來檢查它。
+
+    cy.contains("Pay electric bill")
       .parent()
-      .find('input[type=checkbox]')
-      .check()
+      .find("input[type=checkbox]")
+      .check();
 
-    // Now that we've checked the button, we can go ahead and make sure
-    // that the list element is now marked as completed.
-    // Again we'll use `contains` to find the <label> element and then use the `parents` command
-    // to traverse multiple levels up the dom until we find the corresponding <li> element.
-    // Once we get that element, we can assert that it has the completed class.
-    cy.contains('Pay electric bill')
-      .parents('li')
-      .should('have.class', 'completed')
-  })
+    // 現在我們已經檢查了按鈕，我們可以繼續確保列表元素現在被標記為已完成。
+    // 同樣，我們將使用 `contains` 來查找 <label> 元素，
+    // 然後使用 `parents` 命令在 dom 上遍歷多個級別，直到找到相應的 <li> 元素。
+    // 一旦我們獲得了那個元素，我們就可以斷言它有完整的類。
+    cy.contains("Pay electric bill")
+      .parents("li")
+      .should("have.class", "completed");
+  });
 
-  context('with a checked task', () => {
+  context("with a checked task", () => {
     beforeEach(() => {
-      // We'll take the command we used above to check off an element
-      // Since we want to perform multiple tests that start with checking
-      // one element, we put it in the beforeEach hook
-      // so that it runs at the start of every test.
-      cy.contains('Pay electric bill')
+      // 我們將使用上面使用的命令來檢查一個元素 因為我們要執行多個測試，
+      // 從檢查一個元素開始，我們將它放在 beforeEach 鉤子中，
+      // 以便它在每個測試開始時運行。
+
+      cy.contains("Pay electric bill")
         .parent()
-        .find('input[type=checkbox]')
-        .check()
-    })
+        .find("input[type=checkbox]")
+        .check();
+    });
 
-    it('can filter for uncompleted tasks', () => {
-      // We'll click on the "active" button in order to
-      // display only incomplete items
-      cy.contains('Active').click()
+    it("can filter for uncompleted tasks", () => {
+      // 我們將單擊“活動”按鈕以僅顯示不完整的項目
+      cy.contains("Active").click();
 
-      // After filtering, we can assert that there is only the one
-      // incomplete item in the list.
-      cy.get('.todo-list li')
-        .should('have.length', 1)
+      // 過濾後，我們可以斷言列表中只有一個不完整的項目。
+      cy.get(".todo-list li")
+        .should("have.length", 1)
         .first()
-        .should('have.text', 'Walk the dog')
+        .should("have.text", "Walk the dog");
 
-      // For good measure, let's also assert that the task we checked off
-      // does not exist on the page.
-      cy.contains('Pay electric bill').should('not.exist')
-    })
+      // 為了更好地衡量，我們還斷言我們檢查的任務在頁面上不存在。
+      cy.contains("Pay electric bill").should("not.exist");
+    });
 
-    it('can filter for completed tasks', () => {
-      // We can perform similar steps as the test above to ensure
-      // that only completed tasks are shown
-      cy.contains('Completed').click()
+    it("can filter for completed tasks", () => {
+      // 我們可以執行與上述測試類似的步驟，以確保只顯示已完成的任務
+      cy.contains("Completed").click();
 
-      cy.get('.todo-list li')
-        .should('have.length', 1)
+      cy.get(".todo-list li")
+        .should("have.length", 1)
         .first()
-        .should('have.text', 'Pay electric bill')
+        .should("have.text", "Pay electric bill");
 
-      cy.contains('Walk the dog').should('not.exist')
-    })
+      cy.contains("Walk the dog").should("not.exist");
+    });
 
-    it('can delete all completed tasks', () => {
-      // First, let's click the "Clear completed" button
-      // `contains` is actually serving two purposes here.
-      // First, it's ensuring that the button exists within the dom.
-      // This button only appears when at least one task is checked
-      // so this command is implicitly verifying that it does exist.
-      // Second, it selects the button so we can click it.
-      cy.contains('Clear completed').click()
+    it("can delete all completed tasks", () => {
+      // 首先，讓我們點擊“清除完成”按鈕 `contains` 在這裡實際上有兩個目的。
+      // 首先，它確保按鈕存在於 dom 中。此按鈕僅在選中至少一項任務時出現，
+      // 因此此命令隱式驗證它確實存在。其次，它選擇按鈕以便我們可以單擊它。
 
-      // Then we can make sure that there is only one element
-      // in the list and our element does not exist
-      cy.get('.todo-list li')
-        .should('have.length', 1)
-        .should('not.have.text', 'Pay electric bill')
+      cy.contains("Clear completed").click();
 
-      // Finally, make sure that the clear button no longer exists.
-      cy.contains('Clear completed').should('not.exist')
-    })
-  })
-})
+      // 然後我們可以確保列表中只有一個元素並且我們的元素不存在
+      cy.get(".todo-list li")
+        .should("have.length", 1)
+        .should("not.have.text", "Pay electric bill");
+
+      // 最後，確保清除按鈕不再存在。
+      cy.contains("Clear completed").should("not.exist");
+    });
+  });
+});
