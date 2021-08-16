e57f1eb0dc8546080e92157b25991cc6efc90e65
sasie59 <sasie59@gmail.com>
2021-08-16 23:49:07 +0800

    GMA_32th (第32屆金曲獎)

---
+++ cypress/integration/GMA_32th/gma_32th.spec.js
@@ -0,0 +1,5 @@
+import * as tests from './tests/gma_32th.tests';
+
+describe('前往第32屆金曲獎首頁', () => {
+  tests.GoToGMA_32thTests();
+});
---
+++ cypress/integration/GMA_32th/tests/gma_32th.tests.js
@@ -0,0 +1,6 @@
+
+export const GoToGMA_32thTests = () => {
+  it('should go to GMA_32th index\n(前往第32屆金曲獎首頁)', () => {
+    cy.visit('https://gma.tavis.tw/gm32/index.htm');
+  });
+};
