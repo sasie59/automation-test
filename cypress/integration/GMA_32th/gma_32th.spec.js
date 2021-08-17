e76086593283bdfcd2dc5bffcf99810e030ba844
sasie59 <sasie59@gmail.com>
2021-08-17 23:49:09 +0800

    set 比例

--- cypress/integration/GMA_32th/gma_32th.spec.js
+++ cypress/integration/GMA_32th/gma_32th.spec.js
@@ -1,5 +1,12 @@
 import * as tests from './tests/gma_32th.tests';
 
-describe('前往第32屆金曲獎首頁', () => {
-  tests.GoToGMA_32thTests();
+describe('設定視窗比例', () => {
+  beforeEach(() => {
+    cy.viewport(1200, 800);
+  });
+
+  describe('前往第32屆金曲獎首頁', () => {
+    tests.GoToGMA_32thTests();
+  });
+
 });
