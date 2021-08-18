1af73f3933a34880f74db243333f96742928e8f0
sasie59 <sasie59@gmail.com>
2021-08-18 21:20:08 +0800

    修正描述 - 台鐵個人訂票

--- cypress/integration/TRA/tests/book.ticket.tests.js
+++ cypress/integration/TRA/tests/book.ticket.tests.js
@@ -4,7 +4,7 @@
  * it: 正常執行，如有 only 會省略，如有 skip 則執行
  */
 export const GoToBookingPageTest = () => {
-  it('should go to booking page\n(前往個人訂票頁)', () => {
+  it('should go to booking page\n(預期連結台鐵個人訂票頁面)', () => {
     cy.visit('/tra-tip-web/tip/tip001/tip121/query');
     cy.get('.breadcrumb').should('be.visible');
   });
@@ -19,7 +19,7 @@ const bookTicktetType = [
 ];
 // 我就幫你改前幾個，後面的測試你就自己參考一下我怎麼寫的
 export const MainMenuTests = () => {
-  it('should display main menu \n(應出現選單: 完整, 花東常態實名制, 兩鐵列車, 連假加班實名制, 原住民返鄉, 快速)', () => {
+  it('should display main menu \n(預期應出現選單: 完整, 花東常態實名制, 兩鐵列車, 連假加班實名制, 原住民返鄉, 快速)', () => {
     // 先用 get 抓到選單，定義為 menu, 之後可用 get('@menu') 抓
     cy.get('#tablist').as('menu');
     bookTicktetType.forEach(item => {
@@ -29,7 +29,7 @@ export const MainMenuTests = () => {
 };
 
 export const SwitchQueryTypeTest = () => {
-  it('should booking type can change\n(訂票類型可否正常切換\n快速, 完整, 花東常態實名制, 兩鐵列車, 連假加班實名制, 原住民返鄉)', () => {
+  it('should booking type can change\n(預期訂票類型可否正常切換\n快速, 完整, 花東常態實名制, 兩鐵列車, 連假加班實名制, 原住民返鄉)', () => {
     cy.get('#tablist').as('menu');
 
     const tipTypeList = [
@@ -50,7 +50,7 @@ export const SwitchQueryTypeTest = () => {
 };
 
 export const InputIDTest = () => {
-  it('should display ID input\n(身分證字號輸入格可輸入)', () => {
+  it('should display ID input\n(預期身分證字號輸入格可輸入)', () => {
     cy.get('#pid.form-input.idmember.pid')
       .type('A123456789')
       .should('have.value', 'A123456789');
@@ -58,7 +58,7 @@ export const InputIDTest = () => {
 };
 
 export const ChooseStartStationTest = () => {
-  it('should dispaly counties and cities menu and click startStaton is Toucheng\n(應該出現縣市選單並選擇出發站為宜蘭縣頭城)', () => {
+  it('should dispaly counties and cities menu and click startStaton is Toucheng\n(預期應該出現縣市選單並選擇出發站為宜蘭縣頭城)', () => {
     cy.get('.startStation.ui-autocomplete-input').siblings('.icon.icon-list').click();
     cy.get('a[href*="#mainline"]').should('be.visible');
     cy.get('a[href*="#branchline"]').should('be.visible');
@@ -70,7 +70,7 @@ export const ChooseStartStationTest = () => {
 
 /** @fix: modify to should have.value */
 export const ChooseEndStationTest = () => {
-  it('should dispaly counties and cities menu and click endtStaton is Nangang\n(應該出現縣市選單並選擇抵達站為臺北市南港)', () => {
+  it('should dispaly counties and cities menu and click endtStaton is Nangang\n(預期應該出現縣市選單並選擇抵達站為臺北市南港)', () => {
     cy.get('.endStation.ui-autocomplete-input').siblings('.icon.icon-list').click();
     cy.get('a[href*="#mainline"]').should('be.visible');
     cy.get('a[href*="#branchline"]').should('be.visible');
@@ -83,7 +83,7 @@ export const ChooseEndStationTest = () => {
 };
 
 export const StrokeType = () => {
-  it('should display stroke type\n(應出現行程類型為: 單程，去回)', () => {
+  it('should display stroke type\n(預期應出現行程類型為: 單程，去回)', () => {
     cy.get('.form_title').contains('行程類型').as('type');
     cy.get('@type').should('be.visible');
     cy.get('@type')
@@ -98,7 +98,7 @@ export const StrokeType = () => {
 };
 
 export const SwitchStrokeType = () => {
-  it('should stroke type can change\n(行程類型可否正常切換 單程，去回)', () => {
+  it('should stroke type can change\n(預期行程類型可否正常切換 單程，去回)', () => {
     cy.get('.form_title').contains('行程類型').as('type');
     cy.get('.method-content').as('stroke');
     cy.get('@type')
@@ -120,7 +120,7 @@ export const SwitchStrokeType = () => {
 };
 
 export const BookingMethodType = () => {
-  it('should dispaly booking method type\n(應出現訂票方式類型為: 依車次，依時段)', () => {
+  it('should dispaly booking method type\n(預期應出現訂票方式類型為: 依車次，依時段)', () => {
     cy.get('.form_title').contains('訂票方式').as('type');
     cy.get('@type').should('be.visible');
     cy.get('@type')
@@ -135,7 +135,7 @@ export const BookingMethodType = () => {
 };
 
 export const SwitchBookingMethodType = () => {
-  it('should dispaly booking method type\n(訂票方式類型可否正常切換 依車次，依時段)', () => {
+  it('should dispaly booking method type\n(預期訂票方式類型可否正常切換 依車次，依時段)', () => {
     cy.get('.form_title').contains('訂票方式').as('type');
     cy.get('.method-content').as('stroke');
     cy.get('@type')
@@ -152,7 +152,7 @@ export const SwitchBookingMethodType = () => {
 };
 
 export const ChooseTicketQtyTest = () => {
-  it('should choose tictet quantity\n(選擇張數)', () => {
+  it('should choose tictet quantity\n(預期選擇張數)', () => {
     cy.get('#normalQty.normalSeat.seatQty').clear();
     cy.get('#normalQty.normalSeat.seatQty').type(2);
     cy.get('input[name="normalQty"]').should('have.value', '2');
@@ -160,7 +160,7 @@ export const ChooseTicketQtyTest = () => {
 };
 
 export const ChooseGoCarTypeTest = () => {
-  it('should choose start train type\n(選擇去程時段及車種)', () => {
+  it('should choose start train type\n(預期選擇去程時段及車種)', () => {
     cy.get('.form_title').contains('行程類型').as('type');
     cy.get('@type')
       .siblings('.btn-group')
@@ -185,7 +185,7 @@ export const ChooseGoCarTypeTest = () => {
 };
 
 export const ChooseReturnCarTypeTest = () => {
-  it('should choose return train type and ticket quantity\n(選擇回程時段及車種)', () => {
+  it('should choose return train type and ticket quantity\n(預期選擇回程時段及車種)', () => {
     cy.get('.form_title').contains('行程類型').as('type');
     cy.get('@type')
       .siblings('.btn-group')
@@ -212,7 +212,7 @@ export const ChooseReturnCarTypeTest = () => {
 };
 
 export const BookingTicket = () => {
-  it('should click not RBT booking ticket\n(點擊我不是機器人及訂票)', () => {
+  it('should click not RBT booking ticket\n(預期要點擊訂票)', () => {
     // cy.get('.rc-inline-block').last().click();
     cy.get('.btn.btn-3d').click();
   });
