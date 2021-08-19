d86e34bf2c78fc18b20c266628585e5cf2b0fbfd
sasie59 <sasie59@gmail.com>
2021-08-19 23:31:42 +0800

    列出要測試的項目

--- cypress/integration/GMA_32th/gma_32th.spec.js
+++ cypress/integration/GMA_32th/gma_32th.spec.js
@@ -9,4 +9,52 @@ describe('設定視窗比例', () => {
     tests.GoToGMA_32thTests();
   });
 
+  describe('2021金曲國際音樂節', () => {
+    tests.Into2021GoldenMelodyFestivalTests();
+    // tests.GMF_CarouselTests();
+    // tests.GMF_IntroductionTests();
+    // tests.GMF_SeriesOfActivitiesTests();
+    // tests.GMF_SuccessiveLinksTests();
+    // tests.GMF_UnitTests();
+  });
+
+  describe('金曲國際音樂節主選單', () => {
+    tests.GMF_MainMenuTests();
+    // tests.GMF_AboutGMATests();
+    // tests.GMF_UniversityExchangeTests();
+    // tests.GMF_InternationalForumTests();
+    // tests.GMF_TradeFairTradingCenterTests();
+    // tests.GMF_ShowCaseTests();
+  });
+
+  describe('第32屆金曲獎頒獎典禮', () => {
+    tests.Into32thGoldenMelodyAwardsTests();
+    // tests.GMA_CarouselTests();
+    // tests.GMA_PageNavigationTests();
+    // tests.GMA_UnitTests();
+  });
+
+  describe('金曲獎頒獎典禮主選單', () => {
+    tests.GMA_MainMenuTests();
+    // tests.GMA_BroadcastMessageTests();
+    // tests.GMA_AppLinksTests();
+    // tests.GMA_LiveTests();
+    // tests.GMA_ShortlistedListTests();
+    // tests.GMA_PastWinnersListTests();
+    // tests.GMA_TrailerTests();
+    // tests.GMA_HeatTests();
+    // tests.GMA_CeremonyLocationTests();
+  });
+
+  describe('共同擁有的元素', () => {
+    tests.CommonOwnElementsTests();
+    // tests.BothTitleTests();
+    // tests.LatestNewsTests();
+    // tests.MediaRegistrationTests();
+    // tests.VendorZoneTests();
+    // tests.LanguageSwitchTests();
+    // tests.SocialMediaTests();
+    // tests.GoTopTests();
+  });
+
 });
--- cypress/integration/GMA_32th/tests/gma_32th.tests.js
+++ cypress/integration/GMA_32th/tests/gma_32th.tests.js
@@ -5,3 +5,33 @@ export const GoToGMA_32thTests = () => {
     cy.get('#left').get('#right').should('be.visible');
   });
 };
+
+export const Into2021GoldenMelodyFestivalTests = () => {
+  it('should into GMF page\n(預期進入2021金曲國際音樂節頁面)', () => {
+
+  });
+};
+
+export const GMF_MainMenuTests = () => {
+  it('should display GMF Main Menu\n(預期呈現金曲國際音樂節主選單)', () => {
+
+  });
+};
+
+export const Into32thGoldenMelodyAwardsTests = () => {
+  it('should into GMA page\n(預期進入第32屆金曲獎頒獎典禮)', () => {
+
+  });
+};
+
+export const GMA_MainMenuTests = () => {
+  it('should display GMA Main Menu\n(預期呈現金曲獎頒獎典禮主選單)', () => {
+
+  });
+};
+
+export const CommonOwnElementsTests = () => {
+  it('should common own elements\n(預期共同擁有的元素)', () => {
+
+  });
+};
