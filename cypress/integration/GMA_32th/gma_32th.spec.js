c3531a0b0e4caa2bcb6b7baae7387d7a13621c83
sasie59 <sasie59@gmail.com>
2021-08-20 22:31:44 +0800

    2021金曲國際音樂節 / tests all pass

--- cypress/integration/GMA_32th/gma_32th.spec.js
+++ cypress/integration/GMA_32th/gma_32th.spec.js
@@ -11,11 +11,11 @@ describe('設定視窗比例', () => {
 
   describe('2021金曲國際音樂節', () => {
     tests.Into2021GoldenMelodyFestivalTests();
-    // tests.GMF_CarouselTests();
-    // tests.GMF_IntroductionTests();
-    // tests.GMF_SeriesOfActivitiesTests();
+    tests.GMF_CarouselTests();
+    tests.GMF_IntroductionTests();
+    tests.GMF_SeriesOfActivitiesTests();
     tests.GMF_SuccessiveLinksTests();
-    // tests.GMF_UnitTests();
+    tests.GMF_UnitTests();
   });
 
   describe('金曲國際音樂節主選單', () => {
