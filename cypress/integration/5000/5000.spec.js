6e2ba5d97053c5fb01dd272abfabd07aa239a66b
sasie59 <sasie59@gmail.com>
2021-09-23 23:14:49 +0800

    先列出大綱

--- cypress/integration/5000/5000.spec.js
+++ cypress/integration/5000/5000.spec.js
@@ -12,5 +12,52 @@ describe('振興五倍卷', () => {
   describe('前往振興五倍卷', () => {
     tests.GoTo5000Tests();
   });
+
+  describe('首頁導覽', () => {
+    // tests.HomeTourTests();
+    // tests.ZoomOutAndEnlargetests();
+  });
+
+  describe('使用方式', () => {
+    // tests.TypeToUse();
+    // tests.CreditCardTests();
+    // tests.E_TicketTests();
+    // tests.MobilePaymentTests();
+    // tests.PaperRollTests();
+  });
+
+  describe('各部會加碼', () => {
+    // tests.OverWeight();
+    // tests.GoodForEatTicketTests();
+    // tests.TravelTicketTests();
+    // tests.AboriginalTicketTests();
+    // tests.FarmingTourTicketTests();
+    // tests.ArtFunTicketTests();
+    // tests.DomsTicketTests();
+    // tests.HakkaTicketTests();
+    // tests.LocalCreationTicketTests();
+  });
+
+  describe('預訂與查詢', () => {
+    tests.BookingAndCheck();
+    // tests.PersonalBindingTests();
+    // tests.Co_BindingTests();
+    // tests.QuitCo_BindingTests();
+    // tests.PaperFor5000tests();
+    // tests.OverWeightLogInTests();
+    // tests.CheckTests()
+  });
+
+  describe('影音教學', () => {
+    // tests.AudiovisualTeachingTests();
+  });
+
+  describe('常見問題', () => {
+    // tests.CommonProblemTests();
+  });
+
+  describe('切換語系', () => {
+    // tests.SwitchLanguageTests();
+  });
   
 });
--- cypress/integration/5000/tests/5000.tests.js
+++ cypress/integration/5000/tests/5000.tests.js
@@ -4,5 +4,104 @@ export const GoTo5000Tests = () => {
     cy.wait(2000);
     cy.get('.simple-text.major-logo a').should('have.attr', 'href');
     //title振興五倍卷//
+    cy.get('#Group_5317_twxNNhFwXX').should('be.visible');
   });
+};
+
+export const HomeTourTests = () => {
+  it('should ', ()=> {
+    
+  });
+};
+
+export const ZoomOutAndEnlargetests = () => {
+  it('should ', ()=> {
+
+  });
+};
+
+export const TypeToUse = () => {
+  beforeEach(() => {
+    
+  });
+};
+
+export const CreditCardTests = () => {
+  it('should ', ()=> {
+
+  });
+};
+
+export const E_TicketTests = () => {
+  it('should ', ()=> {
+
+  });
+};
+
+export const MobilePaymentTests = () => {
+  it('should ', ()=> {
+
+  });
+};
+
+export const PaperRollTests = () => {
+  it('should ', ()=> {
+
+  });
+};
+
+export const OverWeight = () => {
+
+};
+
+export const GoodForEatTicketTests = () => {
+  it('should ', ()=> {
+
+  });
+};
+
+export const TravelTicketTests = () => {
+  it('should ', ()=> {
+
+  });
+};
+
+export const AboriginalTicketTests = () => {
+  it('should ', ()=> {
+
+  });
+};
+
+export const FarmingTourTicketTests = () => {
+  it('should ', ()=> {
+
+  });
+};
+
+export const ArtFunTicketTests = () => {
+  it('should ', ()=> {
+
+  });
+};
+
+export const DomsTicketTests = () => {
+  it('should ', ()=> {
+
+  });
+};
+
+export const HakkaTicketTests = () => {
+  it('should ', ()=> {
+
+  });
+};
+
+export const LocalCreationTicketTests = () => {
+  it('should ', ()=> {
+
+  });
+};
+
+export const BookingAndCheck = () => {
+  
 };
