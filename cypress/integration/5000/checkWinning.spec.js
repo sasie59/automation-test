5e2c09f73f420dc94b1f7286e01775aedd9a62b8
sasie59 <sasie59@gmail.com>
2021-10-23 22:24:05 +0800

    新增加碼券中獎查詢測試

---
+++ cypress/integration/5000/checkWinning.spec.js
@@ -0,0 +1,9 @@
+import * as tests from './tests/checkWinning.tests';
+
+Cypress.on("uncaught:exception", (err, runnable) => {
+  return false;
+});
+
+describe('前往加碼券中獎查詢', () => {
+  tests.GoToPlusCouponCheckWinnerTests();
+});
---
+++ cypress/integration/5000/tests/checkWinning.tests.js
@@ -0,0 +1,6 @@
+export const GoToPlusCouponCheckWinnerTests = () => {
+  it('should \n(預期連結加碼券中獎查詢)', () => {
+    cy.visit('https://sexyoung.github.io/vhpi.5000/');
+    cy.wait(1000);
+  });
+};
